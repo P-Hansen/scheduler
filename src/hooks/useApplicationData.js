acc125bc22fc4ed45da9245d5daa53b85258a3b7
P-Hansen <philip.pt.hansen@gmail.com>
2021-04-25 22:52:22 -0600

    removing dead imports

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,9 +1,5 @@
-import DayList from "components/DayList";
-import React, { useState, useEffect } from "react";
-import Appointment from "components/Appointment/index";
-import {getAppointmentsForDay, getInterview, getInterviewersForDay} from "../components/helpers/selectors";
+import { useState, useEffect } from "react";
 import axios from "axios";
-import DayListItem from "components/DayListItem";
 
 export default function useApplicationData() {
     const [state, setState] = useState({
@@ -11,18 +7,16 @@ export default function useApplicationData() {
         days: [],
         appointments: {}
       });
-    
-      //const interviewers = getInterviewersForDay(state, state.day);
-      //const dailyAppointments = getAppointmentsForDay(state, state.day);
+      const baseURL = "http://localhost:8001";
 
+      //load data from the server on mount
       useEffect(()=>{
         Promise.all([
-          axios.get("http://localhost:8001/api/days"),
-          axios.get("http://localhost:8001/api/appointments"),
-          axios.get("http://localhost:8001/api/interviewers")
+          axios.get(baseURL + "/api/days"),
+          axios.get(baseURL + "/api/appointments"),
+          axios.get(baseURL + "/api/interviewers")
         ])
         .then((all)=>{
-          //console.log(all[2].data);
           setState((prev)=>({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
         })
       }, []);
@@ -52,7 +46,6 @@ export default function useApplicationData() {
       }
     
       function bookInterview(id, interview) {
-        console.log("book update", id, interview);
         const appointment = {
           ...state.appointments[id],
           interview: { ...interview }
@@ -63,7 +56,7 @@ export default function useApplicationData() {
           [id]: appointment
         };
         console.log("appointment", appointment);
-        return axios.put(`http://localhost:8001/api/appointments/${id}`, appointment)
+        return axios.put(baseURL + `/api/appointments/${id}`, appointment)
         .then((res)=>{
             setState({
                 ...state,
@@ -73,7 +66,7 @@ export default function useApplicationData() {
       }
     
       function deleteCall(id) {
-        return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+        return axios.delete(baseURL + `/api/appointments/${id}`)
         .then((res)=>{
           console.log("delete response", res);
           const appointment = {
