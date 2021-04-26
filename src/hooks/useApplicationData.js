9539969721d11d4bf97667ce92862f3c3c74d0d0
P-Hansen <philip.pt.hansen@gmail.com>
2021-04-26 15:49:25 -0600

    proxi base URL fix

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -7,14 +7,13 @@ export default function useApplicationData() {
         days: [],
         appointments: {}
       });
-      const baseURL = "http://localhost:8001";
 
       //load data from the server on mount
       useEffect(()=>{
         Promise.all([
-          axios.get(baseURL + "/api/days"),
-          axios.get(baseURL + "/api/appointments"),
-          axios.get(baseURL + "/api/interviewers")
+          axios.get("/api/days"),
+          axios.get("/api/appointments"),
+          axios.get("/api/interviewers")
         ])
         .then((all)=>{
           setState((prev)=>({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
@@ -58,7 +57,7 @@ export default function useApplicationData() {
           ...state.appointments,
           [id]: appointment
         };
-        return axios.put(baseURL + `/api/appointments/${id}`, appointment)
+        return axios.put(`/api/appointments/${id}`, appointment)
         .then((res)=>{
             setState({
                 ...state,
@@ -69,7 +68,7 @@ export default function useApplicationData() {
     
       //updates state and deletes an appointment from the server
       function deleteCall(id) {
-        return axios.delete(baseURL + `/api/appointments/${id}`)
+        return axios.delete(`/api/appointments/${id}`)
         .then((res)=>{
           console.log("delete response", res);
           const appointment = {
