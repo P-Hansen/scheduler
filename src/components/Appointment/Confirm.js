695e86763f3b38b925df1b0e692211897ced5523
P-Hansen <philip.pt.hansen@gmail.com>
2021-04-26 15:48:43 -0600

    confirm style fix

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "components/Button.scss";
+import Button from "components/Button"
 
 export default function Confirm (props) {
 
@@ -11,8 +12,8 @@ export default function Confirm (props) {
     <main className="appointment__card appointment__card--confirm">
         <h1 className="text--semi-bold">{props.message}</h1>
         <section className="appointment__actions">
-            <button onClick={props.onCancel} danger>Cancel</button>
-            <button onClick={confirm} danger>Confirm</button>
+            <Button onClick={props.onCancel} danger>Cancel</Button>
+            <Button onClick={confirm} danger>Confirm</Button>
         </section>
     </main>
     );
